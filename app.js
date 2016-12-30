49b068b0162beecda003b1630dc9d42d2657f984
xiaoyin <liuxiaoyin2016@163.com>
2016-12-30 23:22:07 +0800

    node express mongoose

---
+++ app.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var hbs = require('hbs');
+
+
+var app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// mongo start
+
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://lantu:lantu123@127.0.0.1:27017/lantu");
+
+mongoose.connection.on("error", function (error){
+    console.log("连接数据库失败"+error);
+}).on("open", function (){
+    console.log("数据库连接成功！！！");
+});
+
+var appoint_info_schema = new mongoose.Schema({
+  episode : Date,
+  court : String,
+  valid : Number
+});
+
+var appoint_schema = new mongoose.Schema({
+    customerId : Number,
+    customerName : String,
+    createTime : Date,
+    updateTime : Date,
+    appointInfo : [appoint_info_schema],
+    valid : Number,
+    isPay : Number
+},{collection: "appoint"});
+
+var appoint_model = mongoose.model("appoint", appoint_schema);
+
+// var testEntity = new tmodel({    name: "testUser"});
+// testEntity.save(function (error, doc){    if(error){        console.log("error: "+error);    }else{        console.log(doc);    }});
+
+appoint_model.find({}, function (error, docs){
+    if(error){
+        console.log("error: "+error);
+    }else{
+        console.log(docs);
+        // console.log(docs[0].appointInfo);
+    }
+});
+
+// mongo end
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error_h');
+});
+
+module.exports = app;

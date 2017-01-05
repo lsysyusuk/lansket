5f11b86ee8feb32441ede6ac9f4fe86914de824a
LiShuyang <syusuk@yeah.net>
2017-01-05 23:59:46 +0800

    appoint json

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/customer', customer);
+app.use('/lantu/customer', customer);
 
 // mongo start
 
--- routes/customer.js
+++ routes/customer.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require("mongoose");
+var _ = require('underscore');
+
+var episodeList = [8, 10, 12, 14, 16, 18, 20];
+var courtList = [1, 2, 3, 4];
 
 var appoint_info_schema = new mongoose.Schema({
   episode : Number,
@@ -25,19 +29,48 @@ var appoint_model = mongoose.model("appoint", appoint_schema);
 // var testEntity = new tmodel({    name: "testUser"});
 // testEntity.save(function (error, doc){    if(error){        console.log("error: "+error);    }else{        console.log(doc);    }});
 
-/* GET users listing. */
+var findDisable = function (episode, court, appointJson) {
+   var _court = court;
+    var _episode = episode;
+    var map = _.some(appointJson, function (e) {
+      return _.some(e.appointInfo, function (ee) {
+        if (ee.status == 1 && ee.episode == _episode && ee.court == _court) {
+          return true;
+        } else {
+          return false;
+        }
+      });	
+    })
+    return map;
+}
+
+
 router.get('/appointList.json', function(req, res, next) {
 	var date = req.query.date;
 	var q = {};
 	if (typeof date != "undefined") {
 		q.appointDate = date;
 	}
-  	appoint_model.find(q, function (error, docs){
-    	if(error){
-       		console.log("error: " + error);
-    	}else{
-    		res.send(docs);
-    	}
+	appoint_model.find(q, function (error, docs){
+  	if(error){
+     		console.log("error: " + error);
+  	}else{
+  		console.log(docs);
+
+      var episode_court_map = _.map(episodeList, function (_episode) {
+        return {"episode" : _episode, "courtList" : _.map(courtList, function (_court) {
+          var s = {"court" : _court, "status": 0};
+          if (findDisable(_episode, _court, docs)) {
+            s.status = 2;
+            console.log("----");
+          }
+          return s;
+        })};
+      })
+      console.log(episode_court_map);
+
+  		res.send({"episode_court_map": episode_court_map, "appointJson": docs});
+  	}
 	});
 });
 

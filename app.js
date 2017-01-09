bac2bccc3dc8382ffe4440c670822830ae946b0c
LiShuyang <syusuk@yeah.net>
2017-01-09 22:52:39 +0800

    no message

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', index);
+app.use('/lantu', index);
 app.use('/lantu/customer', customer);
 
 // mongo start
